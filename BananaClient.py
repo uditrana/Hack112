3c292c9ffb2957ec2966464732d05b2f720c6155
amywuwho <amy.wu1579@gmail.com>
2016-11-13 02:12:41 -0500

    tile tray in-progress
    still workin'

--- BananaClient.py
+++ BananaClient.py
@@ -13,11 +13,13 @@ def make2dList(rows, cols, val): #adapted from course notes
 def init(data):
     data.rows = 8
     data.cols = 8
-    data.trayHeight = 1
+    data.tiles = []
+    data.trayRows = int(roundHalfUp(len(data.tiles)/data.cols))
+    data.trayCols = min(len(data.tiles), data.cols)
+    data.tileBoard = updateTileTray(data, data.tiles, make2dList(data.trayRows, data.trayCols, ""))
     data.EMPTY = ""
     data.emptyColor = "light yellow"
     data.fillColor = "gold"
-    data.tiles = []
     data.sWidth = 4
     data.fillWidth = 2
     data.emptyWidth = 1 
@@ -25,6 +27,11 @@ def init(data):
     data.margin = 10 # margin around grid
     data.sRow = 0 #sRow, sCol denotes user-selected cell
     data.sCol = 0
+
+def updateTileTray(data, tiles, tileBoard):
+    for row in range(data.trayRows):
+        for col in range(data.trayCols):
+            
 
 #cell stuff from Course Notes
 def pointInGrid(x, y, data):
@@ -36,11 +43,11 @@ def getCell(x, y, data):
     # aka "viewToModel"
     # return (row, col) in which (x, y) occurred or (-1, -1) if outside grid.
     if (not pointInGrid(x, y, data)):
-        return (-1, -1)
+        return (0, 0)
     gridWidth  = data.width - 2*data.margin
     gridHeight = data.height - 2*data.margin
-    cellWidth  = gridWidth / data.cols
-    cellHeight = gridHeight / data.rows
+    cellWidth  = gridWidth / (data.cols+data.trayCols)
+    cellHeight = gridHeight / (data.rows+data.trayRows)
     row = (y - data.margin) // cellHeight
     col = (x - data.margin) // cellWidth
     # triple-check that we are in bounds
@@ -66,9 +73,9 @@ def getCellBounds(row, col, data):
     # aka "modelToView"
     # returns (x0, y0, x1, y1) corners/bounding box of given cell in grid
     gridWidth  = data.width - 2*data.margin
-    gridHeight = data.height - 2*data.margin
-    columnWidth = gridWidth / data.cols
-    rowHeight = gridHeight / data.rows
+    gridHeight = data.height - 3*data.margin
+    columnWidth = gridWidth / (data.cols)
+    rowHeight = gridHeight / (data.rows+data.trayRows)
     x0 = data.margin + col * columnWidth
     x1 = data.margin + (col+1) * columnWidth
     y0 = data.margin + row * rowHeight
@@ -93,8 +100,16 @@ def drawLetters(canvas, data):
             if data.board[row][col] != data.EMPTY:
                 canvas.create_text((x0+x1)/2, (y0+y1)/2, text=data.board[row][col], font="Helvetica 20")
 
+
 def drawTiles(canvas, data):
-    pass
+    for row in range(data.trayRows):
+        for col in range(data.trayCols):
+            (x0, y0, x1, y1) = getCellBounds(row+data.rows, col, data)
+            fill = data.fillColor
+            width = data.fillWidth
+            if data.tileBoard[row][col] != "": 
+                canvas.create_rectangle(x0, y0+data.margin, x1, y1+data.margin, fill=fill, width=width)
+                canvas.create_text((x0+x1)/2, (y0+y1+2*data.margin)/2, text=data.tileBoard[row][col], font="Helvetica 20")
 
 def drawGrid(canvas, data):
     for row in range(data.rows):
@@ -113,7 +128,7 @@ def drawGrid(canvas, data):
 # use the run function as-is
 ####################################
 
-def run(width=300, height=300):
+def run(width=300, height=347.5):
     def redrawAllWrapper(canvas, data):
         canvas.delete(ALL)
         canvas.create_rectangle(0, 0, data.width, data.height,
@@ -143,7 +158,6 @@ def run(width=300, height=300):
     # Set up data and call init
     class Struct(object): pass
     data = Struct()
-    data.winMargin = 10
     data.width = width
     data.height = height
     data.timerDelay = 100 # milliseconds
@@ -162,9 +176,9 @@ def run(width=300, height=300):
                             keyPressedWrapper(event, canvas, data))
     root.bind("<Configure>", lambda event: sizeChanged(event, data))
     timerFiredWrapper(canvas, data)
-    root.minsize(data.width+data.winMargin, data.height+data.winMargin)
+    root.minsize(data.width, data.height)
     # and launch the app
     root.mainloop()  # blocks until window is closed
     print("bye!")
 
-run(600, 600)
+run(600, 685)
